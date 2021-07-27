ff061e438908de33b9532bc3a395fc8c38a86802
xoxo <ilgongyuk@gmail.com>
2021-07-27 18:56:58 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -7,10 +7,12 @@ const app = express();
 // 라우팅
 const home = require("./src/routes/home");
 
-//앱 세팅
+// 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+// 정적 경로 추가
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home);
 
 module.exports = app;
