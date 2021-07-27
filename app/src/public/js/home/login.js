889a2e1cd2d6594dc7a6460384d4ef5a8670de2a
xoxo <ilgongyuk@gmail.com>
2021-07-27 19:20:17 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,17 +1,24 @@
 "use strict";
 
 const id = document.querySelector("#id"),
-    psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("button");
+	psword = document.querySelector("#psword"),
+	loginBtn = document.querySelector("button");
 
-//login 버튼 클릭시 login함수 실행 
 loginBtn.addEventListener('click', login);
 
 function login () {
-		//req 객체에 id와 psword 값을 넣어 준다. 
-    const req = {
-        id: id.value,
-        psword: psword.value
-    }
-    console.log(req);
+	const req = {
+	  id: id.value,
+	  psword: psword.value
+  };
+
+fetch('/login', {
+	//post 방식으로 데이터 전달.
+	method: "POST",
+	headers: {
+		"Content-Type": "application/json",
+	},
+	//JSON형태로 문자열로 감싸서 데이터 전달. 
+	body: JSON.stringify(req), 
+  });
 }
